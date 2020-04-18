351cfd136c49e6d8f6fd65ade448bf594b1b2a5f
Kumar, Shubham <shubham.kumar7c7c8@cognizant.com>
2020-04-19 04:01:12 +0530

    Added components

--- todo/src/app/app.module.ts
+++ todo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { LoginComponent } from './login/login.component';
+import { ErrorComponent } from './error/error.component';
+import { ListTodosComponent } from './list-todos/list-todos.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    LoginComponent,
+    ErrorComponent,
+    ListTodosComponent
   ],
   imports: [
     BrowserModule,
